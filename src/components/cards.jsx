a10123e774a547e292a0ad644eeeab0444f4bb7d
Glitch (glitch-hello-react) <none>
2021-05-14 00:08:17 +0000

    🐘👩‍👩‍👦 Checkpoint ./src/components/cards.jsx:16097079/8153

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -8,7 +8,11 @@ import Img4 from './img4.jsx'
 import Img5 from './img5.jsx'
 import Img6 from './img6.jsx'
 import '../styles/styles.css'
+
+
 const ImgArray = [<Img1/>,<Img2/>,<Img3/>,<Img4/>,<Img5/>,<Img6/>]
+
+
 const Cardi = (props)=>{
   return(
     <div onClick={reveal} class="card">
@@ -25,21 +29,31 @@ function reveal(e){
     real.classList.add('block')
     real.classList.remove('imgs')
 }
+
+
 function random(){
-  
   var min = Math.ceil(0);
   var max = Math.floor(5);
   var numeroF = Math.floor(Math.random() * (max - min)) + min;
   jafoi.push(numeroF)  
   var limite = false;
+  return numeroF;
+ 
+  
+}
+function newNumber(){
+  var novoNumero = random()
+  var limite;
+  console.log(novoNumero)
   jafoi.map((n,i)=>{
-      
-    if(n == numeroF){
-      limite = true;
-    
+  
+    if(n == novoNumero){
+     limite = true;
     }
+    
+  
   })
-  return numeroF
+  return novoNumero;  
 }
 
 export default function Card(props){
@@ -47,20 +61,20 @@ export default function Card(props){
     
     <div>
       <div class= "painel">
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
       </div>
       <div class='painel'>
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
-        <Cardi num ={random()}/>
-        <Cardi num = {random()}/>
-        <Cardi num ={random()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num ={newNumber()}/>
+        <Cardi num = {newNumber()}/>
+        <Cardi num ={newNumber()}/>
       </div>
      </div>
   )
