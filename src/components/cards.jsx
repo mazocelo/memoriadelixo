f5f0c93b5aae56fb7ea1dc3607df402801abca5b
Glitch (glitch-hello-react) <none>
2021-05-14 01:06:44 +0000

    👾🏵 Checkpoint ./src/components/cards.jsx:16097079/8912

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,66 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+import "../styles/styles.css";
 
-import '../styles/styles.css'
+const src1 =
+  "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FCapturar.PNG?v=1620938384884";
+const src2 =
+  "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FInquerito%20fk%20news.png?v=1620938407826";
+const src3 =
+  "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FSem%20t%C3%ADtulo.png?v=1620938467861";
+const src4 =
+  "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FPortaria%20Eleva%20Sal%C3%A1rio%20de%20bolsonaro.png?v=1620939194426";
+const src5 =
+  "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FBolsonaro%20e%20Mil%C3%ADcia.png?v=1620939289084";
+const src6 =
+  "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2Fluxuria%20de%20bolsonaro.png?v=1620939762682";
 
-const src1 = "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FCapturar.PNG?v=1620938384884"
-const src2 ="https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FInquerito%20fk%20news.png?v=1620938407826"
-const src3 ="https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FSem%20t%C3%ADtulo.png?v=1620938467861"
-const src4 ="https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FPortaria%20Eleva%20Sal%C3%A1rio%20de%20bolsonaro.png?v=1620939194426"
-const src5 ="https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FBolsonaro%20e%20Mil%C3%ADcia.png?v=1620939289084"
-const src6 ="https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2Fluxuria%20de%20bolsonaro.png?v=1620939762682"
-      
+const ImgArray = [src1, src2, src3, src4, src5, src6];
 
+const jafoi = [];
 
-const ImgArray = [src1,src2,src3,src4,src5,src6]
-
-const jafoi =[]
-
-const Cardi = (props)=>{
-  return(
+const Cardi = props => {
+  return (
     <div onClick={reveal} class="card">
-        <img class="imgs card-img"src ={newNumber()}></img>
-</div>
-)}
+      <img class="imgs card-img" src={newNumber()}></img>
+    </div>
+  );
+};
 
 var limite = false;
-var repetidos =[]
+var repetidos = [];
 
-function reveal(e){
-    var real = e.target.childNodes[0]
-    real.classList.add('block')
-    real.classList.remove('imgs')
+function reveal(e) {
+  var real = e.target.childNodes[0];
+  real.classList.add("block");
+  real.classList.remove("imgs");
 }
 
-function randomico(){
+function randomico() {
   var min = Math.ceil(0);
   var max = Math.floor(5);
   var numeroF = Math.floor(Math.random() * (max - min)) + min;
   return numeroF;
 }
 
-function newNumber(){
-  var novoNumero = randomico()
-  
-  jafoi.push(novoNumero)
-  console.log(jafoi)
-  jafoi.map((n,i)=>{
-    if(n == novoNumero){
-      while(n == novoNumero){
-      novoNumero
-      }
-      
+function newNumber() {
+  var novoNumero = randomico();
+  /*jafoi.map((n, i) => {
+    if (n == novoNumero) {
+      limite = true;
     }
-    
-  })
+  });*/
+  jafoi.push(novoNumero);
   return ImgArray[novoNumero];
 }
 
-export default function Card(props){
-  return(
-    
+export default function Card(props) {
+  return (
     <div>
-      <div class= "painel">
+      <div class="painel">
         <Cardi />
         <Cardi />
         <Cardi />
@@ -68,14 +65,14 @@ export default function Card(props){
         <Cardi />
         <Cardi />
       </div>
-      <div class='painel'>
+      <div class="painel">
         <Cardi />
         <Cardi />
         <Cardi />
         <Cardi />
-        <Cardi/>
-        <Cardi/>
+        <Cardi />
+        <Cardi />
       </div>
-     </div>
-  )
+    </div>
+  );
 }
