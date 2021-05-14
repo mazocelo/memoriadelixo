207ea6e4cda692677d754a5c5c6502da3e4c093f
Glitch (glitch-hello-react) <none>
2021-05-14 15:26:20 +0000

    🚍🍱 Checkpoint ./src/components/cards.jsx:16097079/5836

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -15,28 +15,33 @@ const src5 =
 const src6 =
   "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2Fluxuria%20de%20bolsonaro.png?v=1620939762682";
 
-const ImgArray = [src1, src2, src3, src4, src5, src6];
-const ArrayIgual = ImgArray;
+var ImgArray = [src1, src2, src3, src4, src5, src6];
+var ArrayIgual = ImgArray;
 
-const jafoi = [];
+var jafoi = [];
 
 var limite = false;
 var repetidos = [];
-const revealed = false;
+var revealed = false;
 var revealed2 = false;
 
 function reveal(e) {
-  var image = e.target.childNodes[0];
+  
+  //console.log(image)
+  
+}
+function unreveal(e) {
+  if(!revealed){
+    var image = e.target;
   image.classList.remove("viradas");
   image.classList.add("block");
   revealed = true;
-}
-function unreveal(e) {
-  var image = e.target;
+}else
+{  var image = e.target;
   image.classList.add("viradas");
   image.classList.remove("block");
   revealed = false;
-}
+}}
 
 function randomico(i) {
   var min = Math.ceil(0);
@@ -51,25 +56,25 @@ function randomico(i) {
 export default function Card(props) {
   return (
     <div>
-      <div class="painel">
-       
-          {ImgArray.map((img, i) => {
-            return (
-            <div class='div-comp'>
-              <div key={i} onClick={reveal} class="card">
-                <img
+      <div className="painel">
+        {ImgArray.map((img, i) => {
+          return (
+            <div key={i+'d'} className="div-comp">
+              <div key={i+'d2'} onClick={(e)=>{reveal(e)}} className="card">
+                <img key={i+'img'}
                   className="viradas imgs"
-                  onClick={unreveal}
+                  onClick={(e)=>{unreveal(e)}}
                   src={randomico(i)}
                 ></img>
               </div>
-             <a key={i}class="link-img" >reveal link</a>
+              <a key={i + "a"} className="link-img">
+                reveal link
+              </a>
             </div>
-              
-            );
-          })}
-       </div>
-      <div class="painel"></div>
+          );
+        })}
+      </div>
+      <div className="painel"></div>
     </div>
   );
 }
