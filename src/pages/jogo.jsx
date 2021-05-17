cf7480b20614f1fe3144c2bdca9b984c4efb5b7d
Glitch (glitch-hello-react) <none>
2021-05-17 23:53:17 +0000

    💁🚐 Checkpoint ./src/pages/jogo.jsx:16097079/197

--- src/pages/jogo.jsx
+++ src/pages/jogo.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Cards from '../components/cards.jsx'
 export default function Jogo(props){
+  function randomArray(){}
+  
   return( 
     <div className="jogo" >
     
-      <Cards/>
+      <Cards img ={randomArray}/>
      
    
     
