2d10ab72d85cdd94c8740f07b97f4c73c6b25535
Glitch (glitch-hello-react) <none>
2021-05-18 14:27:19 +0000

    💷🚵 Checkpoint ./src/pages/jogo.jsx:16097079/3716

--- src/pages/jogo.jsx
+++ src/pages/jogo.jsx
@@ -1,6 +1,5 @@
-import React, {useState} from 'react'
-import Cards from '../components/cards.jsx'
-
+import React, { useState } from "react";
+import Cards from "../components/cards.jsx";
 
 const src1 =
   "https://cdn.glitch.com/ee2b2e5b-f1c2-458e-83cd-dbede40e5fec%2FCapturar.PNG?v=1620938384884";
@@ -30,43 +29,39 @@ var ImgArray = [
   src6
 ];
 
+export default function Jogo(props) {
+  const [array, setArray] = useState([]);
 
-export default function Jogo(props){
- 
- const[array,setArray] = useState([]) 
-  
-  
-  function randomImg (){
+  function randomImg() {
     var arrayPassageiro = ImgArray;
-    var copyState = ImgArray;
+    var copyState = [];
     var min = Math.ceil(0);
     var max = Math.floor(11);
-    ImgArray.map((img, i) => {
-      var RandomN = Math.floor(Math.random() * (max - i - min)) + min;
-      copyState[i] = arrayPassageiro[RandomN];
-      arrayPassageiro.splice(RandomN, 1);
+    ImgArray.map((img,i) => {
+      var randomN = Math.floor(Math.random() * (max - min)) + min;
+      copyState[i] = arrayPassageiro[randomN];
+      arrayPassageiro.splice(randomN, 1);
+      console.log(i,randomN,copyState)
+      max--
     });
     //copyState.started = true;
     //this.setState(copyState);
     console.log(copyState);
-     setArray(copyState);
-    return copyState;   
-    
+    setArray(copyState);
+    return array;
   }
- 
-  
-  
-  
-  
-  return( 
-    <div className="jogo" >
-      <button onClick={(e)=>{randomImg(e)}}>embaralhar</button>
-      
-      <Cards img ={array}/>
-     
-   
-    
+
+  return (
+    <div className="jogo">
+      <button
+        onClick={e => {
+          randomImg(e);
+        }}
+      >
+        embaralhar
+      </button>
+
+      <Cards img={array} />
     </div>
-    
-)
+  );
 }
