326ef22e718fa117189c915f7693f66a86bb91c4
Glitch (glitch-hello-react) <none>
2021-05-21 13:22:46 +0000

    🍩💰 Checkpoint ./src/components/tempo.jsx:16097079/1269

--- src/components/tempo.jsx
+++ src/components/tempo.jsx
@@ -1,28 +1,27 @@
 import React, { Component } from "react";
 
-
 class Tempo extends Component {
-  constructor(props){
-    super(props)
-    this.state = {tempo: new Date()}
-  }
-  
-  componentDidMount() {
-    setInterval(
-      () => this.tick(),
-      1000
-    );
+  constructor(props) {
+    super(props);
+    this.state = { tempo: new Date() };
   }
 
-  componentWillUnmount() {
+  componentDidMount() {
+    //this.timerID=
+    setInterval(() => this.tick(), 1000);
   }
-  
+  /*
+     componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  */
+
   tick() {
     this.setState({
       tempo: new Date()
     });
   }
-  
+
   render() {
     return (
       <div className="tempo">
