8b497cf36268bc34f0dd4c6493167a10cda1be79
Glitch (glitch-hello-react) <none>
2021-05-21 15:31:53 +0000

    💇💑 Checkpoint ./src/components/tempo.jsx:16097079/6353 ./src/pages/jogo.jsx:16097079/32

--- src/components/tempo.jsx
+++ src/components/tempo.jsx
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 class Tempo extends Component {
   constructor(props) {
     super(props);
-    this.state = { begin: 0 , agora: 0, start: this.props.start };
+    this.state = { begin: this.props.begin, agora: 0, start: this.props.start };
   }
 
   componentDidMount() {
     //this.timerID=
-   setInterval(() => this.tick(), 1000);
-    
+
+    setInterval(() => this.tick(), 1000);
   }
   /*
      componentWillUnmount() {
@@ -17,32 +17,24 @@ class Tempo extends Component {
   }
   */
 
-  componentWillUnmount(){
+  componentWillUnmount() {}
 
-  }
-  
-  
   tick() {
-  
     var novoAgora = this.state.begin - new Date();
     novoAgora = Math.round(novoAgora * 0.001);
     this.setState({
       agora: novoAgora
     });
-    
   }
 
   render() {
-    
     let h2;
     if (this.props.start) {
-       
-      h2=<h2 className="tempo-h">{this.state.agora * -1}s</h2>;
-    }else{
-      h2=<h2 className="tempo-h">0s</h2>;
+      h2 = <h2 className="tempo-h">{this.state.agora * -1}s</h2>;
+    } else {
+      h2 = <h2 className="tempo-h">0s</h2>;
     }
-
-    return <div start = { ()=> {  } } className="tempo">{h2}</div>;
+    return <div className="tempo">{h2}</div>;
   }
 }
 
