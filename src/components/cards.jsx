d49a329dd9be8b93fbdae13c6ea4445e3e1fcd8b
Glitch (glitch-hello-react) <none>
2021-05-22 13:09:55 +0000

    🚝🚌 Checkpoint ./src/components/cards.jsx:16097079/1

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -112,7 +112,7 @@ class Cards extends Component {
   render() {
     let input;
 
-    if (!this.state.win) {
+    if (this.state.win) {
       input = (
         <div id="vitoria" className="vitoria-caixa">
           <h4 className="vitoria-label">
