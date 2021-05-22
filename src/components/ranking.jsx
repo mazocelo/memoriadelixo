9ba2238dcffd41edba5f7c008798aaaec6ba1b3f
Glitch (glitch-hello-react) <none>
2021-05-22 12:33:56 +0000

    ⚽️🏈 Checkpoint ./src/components/ranking.jsx:16097079/1306

--- src/components/ranking.jsx
+++ src/components/ranking.jsx
@@ -1,4 +1,17 @@
 import React, {Component} from 'react'
+import MongoClient from 'mongodb'
+
+
+/*
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://adm:<password>@myind.vcufe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
 
 
 class ranking extends Component{
